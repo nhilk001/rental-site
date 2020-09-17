4a11bb78af7207f28911a3eb3950c306b4e342a1
nshilk001 <60492402+nshilk001@users.noreply.github.com>
2020-09-17 12:30:39 -0400

    not a good branch

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Box from '@material-ui/core/Box';
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
 
-function App() {
+
+const useStyles = makeStyles({
+  table: {
+    minWidth: 650,
+  },
+});
+
+function createData(name, miles, color, seats) {
+  return { name, miles, color, seats };
+}
+
+const rows = [
+  createData('Ford Explorer', 159, 6.0, 24),
+  createData('Toyota camery', 237, 9.0, 37),
+  createData('some other car', 262, 16.0, 24),
+  createData('other car', 305, 3.7, 67),
+  createData('other car also', 356, 16.0, 49),
+];
+ function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <TableContainer component={Paper}>
+      <Table aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Cars for rent</TableCell>
+            <TableCell align="right">miles</TableCell>
+            <TableCell align="right">Color</TableCell>
+            <TableCell align="right">seats</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow key={row.name}>
+              <TableCell component="th" scope="row">
+                {row.name}
+              </TableCell>
+              <TableCell align="right">{row.miles}</TableCell>
+              <TableCell align="right">{row.color}</TableCell>
+              <TableCell align="right">{row.seats}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import * as serviceWorker from './serviceWorker';
 
 const thisIsAnElement = <h1>this is a header</h1>;
 ReactDOM.render(
-  thisIsAnElement,document.getElementById('root')
+  <App />,document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
