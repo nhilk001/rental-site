387d85bd4801396c7dd7df34aa8155e4a2b5f406
nshilk001 <60492402+nshilk001@users.noreply.github.com>
2020-09-22 17:57:32 -0400

    check commit

--- src/index.js
+++ src/index.js
@@ -9,11 +9,8 @@ import SecurityQuestions from './latest/SecurityQuestions';
 import * as serviceWorker from './serviceWorker';
 //this is the main page I believe
 
-ReactDOM.render(<Registration/>,
+ReactDOM.render(<SecurityQuestions/>,
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
